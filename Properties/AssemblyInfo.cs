44cb2604eb1ac2ee4ae46d04d5af1433fc538d6e
Harm Wind <harm.wind@vanoord.com>
2024-02-13 19:37:30 +0100

    Upgrade to .NET Standard 2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,19 +1,8 @@
 ﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("LASzip.Net")]
-[assembly: AssemblyDescription("Classes for reading and writing of LAS/LAZ files. Ported from LASzip.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LASzip.Net")]
-[assembly: AssemblyCopyright("Copyright of C# port © 2014-2019 by Shinta <shintadono@googlemail.com>\r\nCopyright © 2007-2018, martin isenburg <martin.isenburg@rapidlasso.com>, rapidlasso - tools to catch reality <http:\\rapidlasso.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components. If you need to access a type in this assembly from
@@ -22,12 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("984dc190-3bbe-4a0a-b93b-b5a575ac3d32")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("3.2.9.0")]
-[assembly: AssemblyFileVersion("3.2.9.0")]
